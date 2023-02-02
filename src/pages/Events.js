3b6b46b6808a91d1d353d67b583000f2d84653d8
Farmasi Attila <attila.farmasi@dpdgroup.com>
2023-02-02 13:58:08 +0100

    Where should loader() code be stored?

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -12,3 +12,14 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
